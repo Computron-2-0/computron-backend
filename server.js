f9cf8a3ea00432c3df029a2e7cfd756e99ff3ba9
Wayne He <wayneh000@knights.ucf.edu>
2021-01-31 00:18:43 -0500

    Initial work on server API.

---
+++ server.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const { MongoClient, ObjectID } = require("mongodb");
+const Express = require("express");
+const BodyParser = require('body-parser');
+
+const server = Express();
+
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+
+console.log(process.env);
+const client = new MongoClient(process.env.ATLAS_URI);
+
+var collection;
+
+server.listen("3000", async() => {
+    try {
+        await client.connect();
+        collection = client.db("computron").collection("users");
+        console.log("Listening at :3000...");
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+server.post("/signin", async(request, response) => {
+    try {
+      console.log(request.body);
+      var result = await collection.findOne({
+        "user": request.body.username,
+        "pass": request.body.password
+      });
+      if (result == null)
+        response.send({
+          result: null,
+          error: true,
+          message: "Invalid username or password"
+        });
+      else
+        response.send({
+          result: result,
+          error: false,
+          message: "Welcome back, " + result.user + "!"
+        });
+    }
+    catch (e) {
+      response.status(500).send({
+        result: null,
+        error: true,
+        message: e.message
+      });
+    }
+});
