b39d936abad244fea0528435fa13257bddfc4def
Wayne He <wayneh000@knights.ucf.edu>
2021-02-07 03:15:00 -0500

    Endpoints separated into different files.

---
+++ API/signup.js
@@ -0,0 +1,60 @@
+const Express = require("express");
+const server = Express.Router();
+const { MongoClient, ObjectID } = require("mongodb");
+
+server.post("/", async(request, response) => {
+    try {
+        const client = new MongoClient(process.env.ATLAS_URI, { useUnifiedTopology: true });
+        await client.connect();
+        var collection = client.db("computron").collection("users");
+
+        console.log(request.body);
+        var checkUser = await collection.findOne({
+            "user": request.body.username
+        });
+        if (checkUser == null) {
+            //Process user creation if not found.
+            var date = new Date(Date.now());
+            var insertResult = await collection.insertOne({
+                "user": request.body.username,
+                "pass": request.body.password,
+                "email": request.body.email,
+                "date_created": date.toJSON()
+            });
+    
+            if (insertResult == null) {
+                response.status(500).send({
+                    result: null,
+                    error: true,
+                    message: "Error creating user"
+                });
+            } else {
+                var insertCheck = await collection.findOne({
+                    "user": request.body.username
+                });
+                console.log("Return result:\n" + JSON.stringify(insertCheck));
+                response.send({
+                    result: insertCheck,
+                    error: false,
+                    message: "Welcome, " + insertCheck.user + "!"
+                });
+            }
+        } else {
+            //Return error if user is already found.
+            response.send({
+                result: null,
+                error: true,
+                message: "User " + request.body.username + " already exists!"
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        response.status(500).send({
+            result: null,
+            error: true,
+            message: e.message
+        });
+    }
+});
+
+module.exports = server;
